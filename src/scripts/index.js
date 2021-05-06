7f0f9b9e71cfa36c9769cced8b82f35bf6250d1c
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-05-07 01:35:41 +0700

    menambahkan off-canvas-menu atau hamburger menu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,18 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 
 console.log('Hello Coders! :)');
+
+const hamburgerButtonElement = document.querySelector("#hamburger");
+const drawerElement = document.querySelector("#drawer");
+const mainElement = document.querySelector("main");
+
+hamburgerButtonElement.addEventListener("click", (event) => {
+    drawerElement.classList.toggle("open");
+    event.stopPropagation();
+});
+
+mainElement.addEventListener("click", (event) => {
+    drawerElement.classList.remove("open");
+    event.stopPropagation();
+});
+
