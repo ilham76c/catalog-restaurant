1a8df1191d0ba21c6386746bf85768b454434cb4
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-05-07 22:53:51 +0700

    load data restaurants menggunakan javascript

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,14 @@
 import 'regenerator-runtime'; /* for async await transpile */
+import data from "../DATA.json";
 import '../styles/main.css';
+
 
 console.log('Hello Coders! :)');
 
 const hamburgerButtonElement = document.querySelector("#hamburger");
 const drawerElement = document.querySelector("#drawer");
 const mainElement = document.querySelector("main");
+const content = document.getElementById('content');
 
 hamburgerButtonElement.addEventListener("click", (event) => {
     drawerElement.classList.toggle("open");
@@ -17,3 +20,31 @@ mainElement.addEventListener("click", (event) => {
     event.stopPropagation();
 });
 
+let idx = 1;
+let html = '';
+for (const restaurant of data.restaurants) {
+    let card_class = "card d-flex mb-3 bg-white text-black";
+    if (idx % 2 == 0) {
+        card_class += " flex-row-reverse";
+    }
+    let card = /*html*/`
+        <div class="${card_class}">
+            <div class="card-head position-relative">
+                <img src="${restaurant.pictureId}" alt="Avatar">
+                <p class="position-absolute m-1">${restaurant.city}</p>
+            </div>     
+
+            <div class="card-body py-1 px-3">
+                <div class="d-flex justify-content-between">
+                    <h3><a class="decoration-none" href="#">${restaurant.name}</a></h3> 
+                    <p class="my-auto">Rating: ${restaurant.rating}</p>
+                </div>                                    
+                <p class="text-justify">${restaurant.description}</p> 
+            </div>
+        </div>
+        `;
+    html += card;
+    idx++;    
+}
+
+content.innerHTML = html;
