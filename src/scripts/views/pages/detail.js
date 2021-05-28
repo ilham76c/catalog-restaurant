5688bc709430ec2215511aa117bf92e083f0ccb3
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-05-28 22:13:40 +0700

    menambahkan indexDB dan tombol like

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,26 +1,50 @@
-import RestaurantSource from '../../data/resraurant-source';
-import { createRestaurantDetailTemplate } from '../templates/template-creator';
-import UrlParser from '../../routes/url-parser';
+import RestaurantSource from "../../data/resraurant-source";
+
+import {
+    createRestaurantDetailTemplate,
+    createMovieDetailTemplate,
+    createLikeButtonTemplate,
+} from "../templates/template-creator";
+
+import UrlParser from "../../routes/url-parser";
+
+import LikeButtonInitiator from "../../utils/like-button-initiator";
 
 const Detail = {
-  async render() {
-    document.getElementById('jumbotron').classList.add('d-none');
-    return /*html*/`
+    async render() {
+        document.getElementById("jumbotron").classList.add("d-none");
+        return /*html*/ `
         <div id="detail" class="d-flex flex-column px-5">
             <h2 id="content_title" class="mx-auto py-2">Detail Restaurant</h2>
         </div>
+        <div id="likeButtonContainer"></div>
     `;
-  },
+    },
 
-  async afterRender() {
-    
-    const url = UrlParser.parseActiveUrlWithoutCombiner();
-    const restaurant = await RestaurantSource.detailRestaurant(url.id);
-    console.log(restaurant);
-    const restaurantContainer = document.querySelector('#detail');
-    restaurantContainer.innerHTML += createRestaurantDetailTemplate(restaurant);
-    
-  },
+    async afterRender() {
+        const url = UrlParser.parseActiveUrlWithoutCombiner();
+        const restaurant = await RestaurantSource.detailRestaurant(url.id);
+        console.log(restaurant);
+        const restaurantContainer = document.querySelector("#detail");
+        restaurantContainer.innerHTML +=
+            createRestaurantDetailTemplate(restaurant);
+
+        LikeButtonInitiator.init({
+            likeButtonContainer : document.querySelector("#likeButtonContainer"),
+            restaurant          : {
+                                    id              : restaurant.id,
+                                    name            : restaurant.name,
+                                    description     : restaurant.description,
+                                    city            : restaurant.city,
+                                    address         : restaurant.address,
+                                    pictureId       : restaurant.pictureId,
+                                    categories      : restaurant.categories,
+                                    menus           : restaurant.menus,
+                                    rating          : restaurant.rating,
+                                    customerReviews : restaurant.customerReviews,
+                                },
+        });
+    },
 };
 
 export default Detail;
--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -91,4 +91,21 @@ const createCustomerReviews = (customerReviews) => {
     return review;   
 };
 
-export { createRestaurantItemTemplate, createRestaurantDetailTemplate };
+const createLikeButtonTemplate = () => /*html*/ `
+    <button aria-label="like this movie" id="likeButton" class="like">
+        <i class="fa fa-heart-o" aria-hidden="true"></i>
+    </button>
+`;
+ 
+const createLikedButtonTemplate = () => /*html*/ `
+    <button aria-label="unlike this movie" id="likeButton" class="like">
+        <i class="fa fa-heart" aria-hidden="true"></i>
+    </button>
+`;
+
+export {
+    createRestaurantItemTemplate,
+    createRestaurantDetailTemplate,
+    createLikeButtonTemplate,
+    createLikedButtonTemplate,
+};
