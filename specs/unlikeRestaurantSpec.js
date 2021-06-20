69a366b120f7532ca9011e590cec3ed8b8dc6a3e
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-06-20 23:16:43 +0700

    create test case unlike restaurant after liked

---
+++ specs/unlikeRestaurantSpec.js
@@ -0,0 +1,72 @@
+/*eslint-disable no-undef */
+import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator';
+import FavoriteRestaurantIdb from '../src/scripts/data/favorite-restaurant-idb';
+
+const addLikeButtonContainer = () => {
+  document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+};
+
+describe('Unliking A Movie', () => {
+  beforeEach(async () => {
+    addLikeButtonContainer();
+    await FavoriteRestaurantIdb.putRestaurant({ id: 1 });
+  });
+
+  afterEach(async () => {
+    await FavoriteRestaurantIdb.deleteRestaurant(1);
+  });
+
+  it('should display unlike widget when the movie has been liked', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="unlike this movie"]'))
+      .toBeTruthy();
+  });
+
+  it('should not display like widget when the movie has been liked', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="like this movie"]'))
+      .toBeFalsy();
+  });
+
+  it('should be able to remove liked movie from the list', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    document.querySelector('[aria-label="unlike this movie"]').dispatchEvent(new Event('click'));
+
+    expect(await FavoriteRestaurantIdb.getAllRestaurants()).toEqual([]);
+  });
+
+  it('should not throw error if the unliked movie is not in the list', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    //hapus dulu film dari daftar film yang disukai
+    await FavoriteRestaurantIdb.deleteRestaurant(1);
+
+    //kemudian, simulasikan pengguna menekan widget batal menyukai film
+    document.querySelector('[aria-label="unlike this movie"]').dispatchEvent(new Event('click'));
+
+    expect(await FavoriteRestaurantIdb.getAllRestaurants()).toEqual([]);
+  });
+});
