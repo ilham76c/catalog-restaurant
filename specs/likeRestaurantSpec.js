54334a594db52166f5de5c03da38e8066ac7688a
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-06-20 22:20:38 +0700

    membuat test untuk mengecek widget unlike tidak ditampilkan ketika restaurant belum di like

--- specs/likeRestaurantSpec.js
+++ specs/likeRestaurantSpec.js
@@ -1,4 +1,4 @@
-import LikeButtonInitiator from './../src/scripts/utils/like-button-initiator';
+import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator';
 
 describe('Liking A Movie', () => {
   it('should show the like button when the movie has not been liked before', async () => {
@@ -21,5 +21,26 @@ describe('Liking A Movie', () => {
     });
 
     expect(document.querySelector('[aria-label="like this movie"]')).toBeTruthy();
+  });
+
+  it('should not show the unlike button when the movie has not been liked before', async () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+        name: 'restaurant.name',
+        description: 'restaurant.description',
+        city: 'restaurant.city',
+        address: 'restaurant.address',
+        pictureId: 'restaurant.pictureId',
+        categories: 'restaurant.categories',
+        menus: 'restaurant.menus',
+        rating: 'restaurant.rating',
+        customerReviews: 'restaurant.customerReviews',
+      },
+    });
+
+    expect(document.querySelector('[aria-label="unlike this movie"]')).toBeFalsy();
   });
 });
