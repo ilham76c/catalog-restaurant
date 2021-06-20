a1e571e79e0a32e744c7949496934264abd02d9f
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-06-20 22:05:54 +0700

    create like restaurant test

---
+++ specs/likeRestaurantSpec.js
@@ -0,0 +1,25 @@
+import LikeButtonInitiator from './../src/scripts/utils/like-button-initiator';
+
+describe('Liking A Movie', () => {
+  it('should show the like button when the movie has not been liked before', async () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+        name: 'restaurant.name',
+        description: 'restaurant.description',
+        city: 'restaurant.city',
+        address: 'restaurant.address',
+        pictureId: 'restaurant.pictureId',
+        categories: 'restaurant.categories',
+        menus: 'restaurant.menus',
+        rating: 'restaurant.rating',
+        customerReviews: 'restaurant.customerReviews',
+      },
+    });
+
+    expect(document.querySelector('[aria-label="like this movie"]')).toBeTruthy();
+  });
+});
