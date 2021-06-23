cd47424236c59dfa641ae275b0bfb0dd30c922ab
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-06-24 00:07:56 +0700

    menambahkan global image url ukuran medium dan small

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,7 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
-  BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
+  BASE_IMAGE_URL_MEDIUM: 'https://restaurant-api.dicoding.dev/images/medium/',
+  BASE_IMAGE_URL_SMALL: 'https://restaurant-api.dicoding.dev/images/small/',
   CACHE_NAME: new Date().toISOString(),
   DATABASE_NAME: 'catalog-restaurant-database',
   DATABASE_VERSION: 1,
