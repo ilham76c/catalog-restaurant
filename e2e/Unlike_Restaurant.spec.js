bf2d96a5e97f9d7ad30e363877e2725718ac1863
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-06-23 22:12:41 +0700

    add test scenario unlike a restaurant

---
+++ e2e/Unlike_Restaurant.spec.js
@@ -0,0 +1,25 @@
+/*eslint-disable no-undef */
+Feature('Unlike a Restaurant');
+
+Scenario('unlike one restaurant', async ({ I }) => {
+  I.amOnPage('/');
+
+  I.seeElement('#restaurants a');
+
+  I.click(locate('#restaurants a').first());
+
+  pause();
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/like');
+  I.seeElement('#favorite a');
+
+  I.click(locate('#favorite a').first());
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/like');
+  I.seeElement('#favorite');
+  I.see('No favorite restaurant', '#favorite p');
+});
