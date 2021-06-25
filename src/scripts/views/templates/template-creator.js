f8302cd402bf9a210c68a2c59be73eaecf056407
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-06-25 23:50:57 +0700

    mengganti style gambar detail restaurant

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -3,10 +3,9 @@ import CONFIG from '../../globals/config';
 const createRestaurantItemTemplate = (restaurant) => /*html*/ `
   <section class="card d-flex mb-3 bg-white text-black">
     <div class="card-head position-relative">
-      <picture>
-        <source media="(max-width: 767px)" srcset="${CONFIG.BASE_IMAGE_URL_SMALL + restaurant.pictureId}">
+      
         <img class="lazyload" src="${CONFIG.BASE_IMAGE_URL_MEDIUM + restaurant.pictureId}" alt="Gambar ${restaurant.name}" crossorigin="anonymous">
-      </picture>
+      
       <p class="position-absolute m-1">${restaurant.city}</p>
     </div>     
 
@@ -62,10 +61,13 @@ const createCustomerReviews = (customerReviews) => {
 const createRestaurantDetailTemplate = (restaurant) => /*html*/ `
   <section class="card d-flex flex-column mb-3 bg-white text-black">
     <div class="d-flex w-100">
-      <picture>
-        <source media="(max-width: 767x)" srcset="${CONFIG.BASE_IMAGE_URL_SMALL + restaurant.pictureId}">
+      
+      <div class="card-head position-relative w-100">
+      
         <img class="lazyload" src="${CONFIG.BASE_IMAGE_URL_MEDIUM + restaurant.pictureId}" alt="Gambar ${restaurant.name}" crossorigin="anonymous">
-      </picture>
+        <p class="position-absolute m-1">${restaurant.city}</p>
+      </div>     
+
       <div class="mx-1 w-100">                
         <p><b>Name</b>&emsp;&emsp;&emsp; : ${restaurant.name}</p>
         <p><b>Rating</b>&emsp;&emsp;&emsp;: ${restaurant.rating}</p>
