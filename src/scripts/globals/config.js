1691d300c1296139c398de98729c1a6e175fd031
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-11-12 00:42:11 +0700

    mengubah BASE_URL

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  BASE_URL: 'https://restaurant-api.dicoding.dev/',
+  BASE_URL: 'https://restaurant-api.dicoding.dev',
   BASE_IMAGE_URL_MEDIUM: 'https://restaurant-api.dicoding.dev/images/medium/',
   BASE_IMAGE_URL_SMALL: 'https://restaurant-api.dicoding.dev/images/small/',
   CACHE_NAME: new Date().toISOString(),
